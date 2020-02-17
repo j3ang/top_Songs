3d23b9db11fc9e2cadad756af30e18957dcfd2e4
Alex Wang <j3ang@gmail.com>
2020-02-16 20:13:49 -0600

    push

---
+++ R/dplyr_fxns.R
@@ -0,0 +1,119 @@
+###################################################################
+#
+# https://www.rdocumentation.org/packages/dplyr/versions/0.7.8
+# https://rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
+# https://newsroom.spotify.com/2019-12-03/the-top-songs-artists-playlists-and-podcasts-of-2019-and-the-last-decade/
+# https://dplyr.tidyverse.org/
+# Utelizing functions in dplyr package
+#
+###################################################################
+
+#' ######################################################
+#' POP value is the popularity of the artist.
+#' The value will be between 0 and 100, with 100 being the most popular.
+#' The artist’s popularity is calculated from the popularity of all the artist’s tracks.
+#' With no steaming data available in this dataset.
+#' The artist's popularity are deternmined by the weighted value:
+#'
+#' 1. POP value of the artist at that time * (20%)
+#' 2. The number of songs of the artist appeared on the list of that year (80%)
+#' #####################################################
+#'
+#' This function returns top artist of the year
+#'
+#' @param dataset The dataset
+#' @param y The year
+#' @examples
+#' top_artist_yr(dataset, 2012)
+#' @export
+#'
+get_topArtistsBy_yr <- function(dataset, y) {
+  select_all(dataset) %>%
+    group_by(artist) %>%
+    filter(year == y) %>%
+    summarise(
+      max_pop = max(pop),
+      min_pop = min(pop),
+      sum_pop = sum(pop),
+      mean_pop = mean(pop),
+      year = as.character(y),
+      n = n(),
+      eval_pop_of_yr = (n*.8 +  mean_pop*.2)/nrow(.) ) %>%
+    arrange(desc(eval_pop_of_yr)) -> top_artist
+  return(top_artist)
+}
+
+
+
+#' This function returns all top 10 artists of past 10 years
+#'
+#' @param dataset The dataset
+#' @return conbined results of top_artist_yr()
+#' @examples
+#' top_artist_yr_all(dataset)
+#' @export
+#'
+get_topArtists <- function(dataset) {
+  datalist= list()
+  for (i in (max(dataset$year):min(dataset$year))){
+    datalist[[i]] <- head(assign(paste0("top_artist_yr_", i),
+                                 get_topArtistsBy_yr(dataset, as.character(i))),10)
+  }
+  all_top_artists_of_the_yr = do.call(rbind, datalist)  # join all the table
+  return(all_top_artists_of_the_yr)
+}
+
+#' This function returns the most popular songs over the decade
+#' @param dataset The dataset
+#' @return songs with pop value over 80
+#' @examples
+#' get_popSongs(songs)
+#' @export
+#'
+get_popSongs<- function(dataset) {
+  select_all(dataset) %>%
+    filter( pop > 80) %>%
+    arrange(desc(pop)) -> most_pop
+  return(most_pop)
+}
+
+
+
+#' This function returns top dance songs
+#' evaluated by (dnce by .3 + pop by .4  + nrgy by .3)
+#'
+#' The dancibility are re-evaluated on top of original
+#' dnce value with weight given to {nrgy} and {bpm}
+#' @param dataset The dataset
+#' @examples
+#' top_dnce_by_year(songs, 2012)
+#' @export
+#'
+get_topDnce <- function(dataset) {
+ select_all(dataset) %>%
+    group_by(title, artist, year, pop, dnce,nrgy, bpm, val,  dB, dur, acous, spch) %>%
+    filter( top.genre == 'dance pop' & dnce > 75 ) %>%
+    summarise(eval=(dnce*.3 + pop*.4  + nrgy*.3)) %>%
+    arrange(desc(eval)) -> topDnce
+  return(topDnce)
+}
+
+#' This function returns top dance songs by year
+#' evaluated by (dnce by .3 + pop by .4  + nrgy by .3)
+#'
+#' The dancibility are re-evaluated on top of original
+#' dnce value with weight given to {nrgy} and {bpm}
+#' @param dataset The dataset
+#' @param yr The year
+#' @examples
+#' top_dnce_by_year(songs, 2012)
+#' @export
+#'
+get_topDnceBy_yr <- function(dataset,y) {
+  topDnce_all <- get_topDnce(dataset)
+  topDnce_all %>%
+    filter(year == y) -> topDnce_Yr
+  return(topDnce_Yr)
+}
+
+
---
+++ R/psych_fxns.R
@@ -0,0 +1,142 @@
+###################################################################
+#
+# https://www.rdocumentation.org/packages/psych/versions/1.9.12.31
+# https://personality-project.org/r/psych/psych-manual.pdf
+# https://cran.r-project.org/web/packages/psych/vignettes/intro.pdf
+# Utelizing functions in psych package
+#
+###################################################################
+
+#' This function describe data using psych's basic functions
+#'
+#' @param dataset A dataset
+#' @return result table of described dataset
+#' @export
+desc_data <- function(dataset) {
+  print(dim(dataset))
+  title('Described Data')
+  psych_desc <- describe(dataset)
+  print(psych_desc)
+
+  cat('\n\n')
+  title('Head/Tail Data')
+  print(headTail(dataset))
+
+
+  # Basic histograms/dencity
+  num_col <- get_num_col(dataset)
+  class(num_col$bpm)
+  jpeg("dencity_hist.jpg")
+  par(mfrow=c(3,4))
+  # draw histograms of the numerical cols
+  for(name in names(num_col)){
+    hist( num_col[,name], col='grey', freq=FALSE, main=paste0('Histogram of ', name), xlab = paste0(name))
+    lines(density(num_col[,name])) # add density lines
+  }
+  dev.off() # turning off graphic device
+
+
+  return(psych_desc)
+}
+
+
+
+
+#' This function retrives and peeks numeric columns data
+#' @param dataset A dataset
+#' @return dataset with numerical columns
+#' @example
+#' get_num_col(dataset)
+#' @export
+#'
+get_num_col <- function(dataset){
+  title('Peek Numeric Columns')
+  dataset.num.col <- dataset[,sapply(dataset, is.numeric)] # Get all the numeric col for correlation calculation
+  dataset.num.col <- dataset.num.col[2:12] # remove X col
+  print(headTail(dataset.num.col)) # preview top/tail rows
+
+  return(dataset.num.col)
+}
+
+
+
+
+#' This function prints corr test result using corr.test() to
+#' Find the correlations, sample sizes,  and probability values
+#' between elements of a matrix or data.frame.
+#' @param dataset A dataset
+#' @return results table of corr.test()
+#' @example
+#' corrTest(dataset)
+#' @export
+#'
+testCorr <- function(dataset.num.col){
+  title('Corr Test')
+  corr_test <- corr.test(dataset.num.col)
+  print(corr_test)
+
+  return(corr_test)
+}
+
+
+
+#' This function prints corr test result using corr.test() to
+#' Find and print (usinglowerMat) the lower diagonal correlation matrix
+#' but returns (in-visibly) the full correlation matrix found with the use
+# and method parameters.
+#' @param dataset A dataset
+#' @return results table of corr.test()
+#' @example
+#' corrTest(dataset)
+#' @export
+#'
+lowerCorr <- function(dataset){
+  cat("\n")
+  title('Lower Correlation')
+  lowerCor <- lowerCor(dataset, digits=3, method='pearson')
+
+  return(lowerCor)
+}
+
+
+
+#' This function uses:
+#' - paris.panels()
+#' - corPlot()
+#' to graph the correlations to scatter plot and heatmap
+#' @return It will create 2 png files of each
+#' @examples
+#' draw_Corr(num_col_dataset, lowerCor_var)
+#' @export
+#'
+draw_Corr <- function(dataset.num.col, lowerCor){
+
+  png('pars_panels.png')
+  pairs.panels(dataset.num.col) # whows scatter plot matrices and as well as histograms and the Pearson correlation
+  dev.off()
+
+  png('corPlot.png')
+  gr <- colorRampPalette(c("#B52127", "white", "#2171B5"))
+  corPlot(lowerCor,numbers=TRUE,upper=FALSE,diag=FALSE,gr=gr, main="Song Properties Correlations")  # Plot heatmap
+  dev.off()
+}
+
+
+
+
+
+
+
+
+#' This function prints divider with title
+#'
+#' @examples
+#' top_dnce_by_year(songs, 2012)
+#' @export
+#'
+title <- function(title){
+  cat('-------------------------------------------------------------------------------------------------------------\n')
+  cat('                                    ', eval(title) , '\n')
+  cat('-------------------------------------------------------------------------------------------------------------\n')
+}
+
